7b573d741d96018f91485b80812c38b75bfaca8a
Thomas Brannan <th.c.brannan@gmail.com>
2021-09-24 17:02:22 -0700

    Wrapper div for TodoList

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Axios from 'axios';
 
+import TodoList from './TodoList.jsx';
+
 class App extends React.Component {
 
   constructor(props) {
@@ -26,14 +28,18 @@ class App extends React.Component {
     .catch((err) => {
       console.log(`Error in App.jsx::App.componentDidMount()`);
       console.log(err);
-    })
+    });
   }
 
   render() {
     if (this.isLoading) {
       return (<div><h1>From src/components/app.jsx</h1><h2>Relax</h2></div>);
     } else {
-      return (<div><h1>From src/components/app.jsx</h1><h2>Loaded</h2></div>);
+      return (
+        <div id={"app-wraps-todolist"} className={"wrapper"}>
+          <TodoList todoItems={this.state.todoItems} />
+        </div>
+      );
     }
   }
 
