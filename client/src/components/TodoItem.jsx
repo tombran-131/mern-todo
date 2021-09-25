eb71d115c2b82beaa49344fa68d3823cd96dfa53
Thomas Brannan <th.c.brannan@gmail.com>
2021-09-24 17:16:40 -0700

    TodoList and TodoItem reference each other, TodoList maps its todoItems prop to TodoItem components

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const TodoItem = (props) => {
-
-};
+const TodoItem = (props) => (
+  <div></div>
+);
 
 module.exports = TodoItem;
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const TodoList = (props) => {
+import TodoItem from './TodoItem.jsx';
 
-};
+const TodoList = (props) => (<div>{
+  props.todoItems.map((itemData, itemId) => 
+    (<TodoItem data={itemData} id={itemId} />)
+  )
+}</div>);
 
 module.exports = TodoList;
